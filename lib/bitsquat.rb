581022291d8a45ea9b0380b0b3b17a9a36bd9296
Chase McCarthy <chase@code0100fun.com>
2016-10-09 13:52:18 -0400

    Compute bit squat URLs for subdomain and domain

--- lib/bitsquat.rb
+++ lib/bitsquat.rb
@@ -1,5 +1,53 @@
 require "bitsquat/version"
 
 module Bitsquat
-  # Your code goes here...
+  def self.list(url)
+    list = []
+    uri = parse_url(url)
+    host = uri.host.downcase
+    segments = host.split('.')
+    tld = segments[-1]
+    domain = segments[-2]
+    subdomains = segments.reverse[2..-1]
+    subdomain = subdomains.first
+    if subdomain
+      list << "#{subdomain}n#{domain}.#{tld}"
+    end
+    domain.length.times do |i|
+      flipped_domains = flip_bits(domain, i)
+      flipped_domains.each do |flipped_domain|
+        list << "#{flipped_domain}.#{tld}"
+      end
+    end
+    list
+  rescue URI::Error => e
+    []
+  end
+
+  private
+
+  def self.flip_bits(str, char_index)
+    str_bits = str.split('').map { |c| c.unpack('B*').first }.freeze
+    char_bits = str_bits[char_index].split('').map(&:to_i).freeze
+    char_bits.each_with_index.map do |b, i|
+      char_bits_dup = char_bits.dup
+      char_bits_dup[i] = [1,0][b]
+      str_bits_dup = str_bits.dup
+      new_char = char_bits_dup.join('')
+      new_char_code = new_char.to_i(2)
+      if ((new_char_code >= 48 && new_char_code <= 57) || # 0-9
+          (new_char_code >= 97 && new_char_code <= 122) || # a-z
+           new_char_code == 45) # -
+        str_bits_dup[char_index] = new_char
+        [str_bits_dup.join('')].pack('B*')
+      end
+    end.compact
+  end
+
+  def self.parse_url(url)
+    unless url.match(/^(https:\/\/|http:\/\/)/)
+      url = "https://#{url}"
+    end
+    URI.parse(url)
+  end
 end
--- spec/bitsquat_spec.rb
+++ spec/bitsquat_spec.rb
@@ -1,11 +1,49 @@
 require 'spec_helper'
 
 describe Bitsquat do
-  it 'has a version number' do
-    expect(Bitsquat::VERSION).not_to be nil
+
+  it 'uses last subdomain with . repaced by n' do
+    urls = Bitsquat.list('aws.amazon.com')
+    expect(urls).to include('awsnamazon.com')
+    urls = Bitsquat.list('something.aws.amazon.com')
+    expect(urls).to include('awsnamazon.com')
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'checks all letters in the domain name' do
+    urls = Bitsquat.list('amazon.com')
+    expect(urls.length).to eq(24)
+
+    expect(urls).to include(
+      # a
+      'qmazon.com',
+      'imazon.com',
+      'emazon.com',
+      'cmazon.com',
+      # m
+      'a-azon.com',
+      'aeazon.com',
+      'aiazon.com',
+      'aoazon.com',
+      'alazon.com',
+      # a
+      'amqzon.com',
+      'amizon.com',
+      'amezon.com',
+      'amczon.com',
+      # z
+      'amajon.com',
+      'amaron.com',
+      'amaxon.com',
+      # o
+      'amazgn.com',
+      'amazkn.com',
+      'amazmn.com',
+      'amaznn.com',
+      # n
+      'amazof.com',
+      'amazoj.com',
+      'amazol.com',
+      'amazoo.com',
+    )
   end
 end
